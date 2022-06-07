2e0ca01d522fd846c0e7bd063b0953995b78f727
DipeshLama <dipeslama32@gmail.com>
2022-06-07 09:31:24 +0545

    Implemented curved navigation bar

--- app/src/main/java/com/example/apicoroutines/feature/main/MainActivity.kt
+++ app/src/main/java/com/example/apicoroutines/feature/main/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
@@ -24,6 +25,7 @@ import com.example.apicoroutines.feature.shared.base.BaseArrayResponse
 import com.example.apicoroutines.feature.shared.model.response.Home
 import com.example.apicoroutines.utils.resource.Resource
 import com.example.apicoroutines.utils.resource.Status
+import com.qamar.curvedbottomnaviagtion.CurvedBottomNavigation
 import dagger.hilt.android.AndroidEntryPoint
 import org.w3c.dom.Text
 import retrofit2.Response
@@ -37,16 +39,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        initController()
+//        initController()
+        initNavHost()
+        binding.setUpBottomNavigation()
+    }
+    private fun initNavHost() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.frgHome) as NavHostFragment
+        navController = navHostFragment.navController
     }
 
-    private fun initController() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.frgHome)
-        navController = navHostFragment!!.findNavController()
-        AppBarConfiguration(navController.graph)
-        binding.btmNav.setupWithNavController(navController)
-        setupActionBarWithNavController(navController)
+    private fun ActivityMainBinding.setUpBottomNavigation() {
+        val bottomNavigationItems = mutableListOf(
+            CurvedBottomNavigation.Model(HOME_ITEM, getString(R.string.home), R.drawable.ic_home_iconly),
+            CurvedBottomNavigation.Model(CATEGORIES_ITEM, getString(R.string.categories), R.drawable.ic_category_iconly),
+            CurvedBottomNavigation.Model(FAQ_ITEM, getString(R.string.offers), R.drawable.ic_offers),
+            CurvedBottomNavigation.Model(FAVOURITE_ITEM, getString(R.string.favourite), R.drawable.ic_heart),
+            CurvedBottomNavigation.Model(MORE_ITEM, getString(R.string.more), R.drawable.ic_more)
+        )
+        binding.btmNav.apply {
+            bottomNavigationItems.forEach { add(it) }
+            setOnClickMenuListener {
+                navController.navigate(it.id)
+            }
+            // optional
+            setupNavController(navController)
+        }
     }
+
+//    private fun initController() {
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.frgHome)
+//        navController = navHostFragment!!.findNavController()
+//        AppBarConfiguration(navController.graph)
+//        binding.btmNav.setupWithNavController(navController)
+//        setupActionBarWithNavController(navController)
+//    }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
@@ -73,5 +99,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        // you can put any unique id here, but because I am using Navigation Component I prefer to put it as
+        // the fragment id.
+        const val HOME_ITEM = R.id.homeFragment
+        const val CATEGORIES_ITEM = R.id.categoriesFragment
+        const val FAQ_ITEM = R.id.faqFragment
+        const val FAVOURITE_ITEM = R.id.favouriteFragment
+        const val MORE_ITEM = R.id.moreFragment
     }
 }
