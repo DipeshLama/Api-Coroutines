677ae81985f5cfcef55e00bbb9b79f5a1ce906bc
DipeshLama <dipeslama32@gmail.com>
2022-06-22 13:30:38 +0545

    Hide and show bottom nav bar in scroll

--- app/src/main/java/com/example/apicoroutines/feature/googleMaps/MapFragment.kt
+++ app/src/main/java/com/example/apicoroutines/feature/googleMaps/MapFragment.kt
@@ -1,8 +1,6 @@
 package com.example.apicoroutines.feature.googleMaps
 
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
 import android.content.IntentSender
 import android.location.Address
 import android.location.Geocoder
@@ -10,7 +8,6 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.view.inputmethod.EditorInfo
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.navArgs
--- app/src/main/java/com/example/apicoroutines/feature/shared/viewHolder/FaqViewHolder.kt
+++ app/src/main/java/com/example/apicoroutines/feature/shared/viewHolder/FaqViewHolder.kt
@@ -8,7 +8,7 @@ import com.example.apicoroutines.feature.shared.model.response.Faq
 class FaqViewHolder(
     val binding: LayoutFaqListBinding,
     val listener: OnExpandListener,
-    val size: Int
+    val size: Int,
 ) :
     RecyclerView.ViewHolder(binding.root) {
 
@@ -21,5 +21,13 @@ class FaqViewHolder(
     fun bind(faq: Faq) {
         binding.faq = faq
         binding.executePendingBindings()
+
+        if (faq.isExpanded) {
+            binding.txvFaqAns.animate().setDuration(500).alpha(1.0f)
+            binding.imvExpand.animate().rotation(180f)
+        } else {
+            binding.txvFaqAns.animate().setDuration(500).alpha(0.0f)
+            binding.imvExpand.animate().rotation(0f)
+        }
     }
 }
