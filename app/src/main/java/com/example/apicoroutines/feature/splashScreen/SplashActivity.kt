7ba620a455bbd94c6e8a8eee2e114fa86ba94cfc
DipeshLama <dipeslama32@gmail.com>
2022-06-22 15:30:04 +0545

    Added splashscreen and animation in the splash screen

---
+++ app/src/main/java/com/example/apicoroutines/feature/splashScreen/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.example.apicoroutines.feature.splashScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import androidx.databinding.DataBindingUtil
+import com.example.apicoroutines.R
+import com.example.apicoroutines.databinding.ActivitySplashBinding
+import com.example.apicoroutines.feature.main.MainActivity
+import com.example.apicoroutines.utils.router.Router
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        rotateHandler()
+        splashDelayHandler()
+    }
+
+    private fun splashDelayHandler() {
+        Handler().postDelayed({
+            navigateToLanding()
+        }, 4000)
+    }
+
+    private fun navigateToLanding() {
+        Router.navigateActivity(this, true, MainActivity::class)
+    }
+
+    private fun rotateHandler() {
+        val rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_splash)
+        binding.imvOuterLogo.startAnimation(rotateAnimation)
+    }
+}
